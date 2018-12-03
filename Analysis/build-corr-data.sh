2b5d831f6476b1b066546b1a5f1beb9076dd07f2
sanvishal <vishalsanthosh1999@gmail.com>
2018-12-03 12:26:36 +0530

    Fully automated devops for generating data from given data sets

---
+++ Analysis/build-corr-data.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# filename: build-corr-data.sh
+cd ..
+cd Exoplanet-Data
+python correlation.py
--- Exoplanet-Data/correlation.py
+++ Exoplanet-Data/correlation.py
@@ -5,13 +5,17 @@ import numpy as np
 planetData = pd.read_csv("original-data/planets.csv")
 # lots of missing data!!!!!!!!!!!!!!!
 # pprint(planetData.isnull().sum())
-
+print("original data read...")
 
 features = ['PlanetaryMassJpt', 'RadiusJpt', 'PeriodDays', 'SemiMajorAxisAU', 'Eccentricity',
             'SurfaceTempK', 'HostStarMassSlrMass', 'HostStarRadiusSlrRad',
             'HostStarMetallicity', 'HostStarTempK', 'HostStarAgeGyr']
 
 matrix = planetData[features].dropna()
+print("dropping all the null values...")
 
-pprint(matrix.to_csv("../Analysis/corr-data/corr.csv"))
-pprint(matrix.to_csv("../Analysis/corr-data/scatter.csv"))
+
+matrix.to_csv("../Analysis/public/corr-data/corr.csv")
+matrix.to_csv("../Analysis/public/corr-data/scatter.csv")
+
+print("\033[1m"+"csv exported to public/corr-data"+"\033[0m")
