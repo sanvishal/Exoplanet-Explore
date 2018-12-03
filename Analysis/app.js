53466a1af58155de142a7b1e7cee83cadc768f78
sanvishal <vishalsanthosh1999@gmail.com>
2018-12-03 17:13:54 +0530

    Added favion, now the website look 10000 times better

--- Analysis/app.js
+++ Analysis/app.js
@@ -2,3 +2,4 @@ var express = require("express");
 var server = express();
 server.use("/", express.static(__dirname + "/public"));
 server.listen(8080);
+console.log("go to localhost:8080 in your browser");
