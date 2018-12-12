94556d7e18baba1ec8d4fca9a8f5921ffafef56e
sanvishal <vishalsanthosh1999@gmail.com>
2018-12-12 20:55:02 +0530

    added comments for one file

--- Analysis/app.js
+++ Analysis/app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var server = express();
+//just an ordinary static file server using express.js
 server.use("/", express.static(__dirname + "/public"));
 server.listen(8080);
-console.log("go to localhost:8080 in your browser");
+console.log("Go to localhost:8080 in your browser");
--- Analysis/public/js/dist-chart.js
+++ Analysis/public/js/dist-chart.js
@@ -1,11 +1,19 @@
 var width = 800,
 	height = 800;
 
+//setup scales
 var rScale = d3
 	.scaleLinear()
 	.domain([3.5, 200])
 	.range([7, 3]);
 
+var x = d3.scaleLinear()
+	.range([0, width]),
+
+	y = d3.scaleLinear()
+		.range([0, height])
+
+//the main SVG container
 var distSVG = d3
 	.select("#dist-chart")
 	.attr("class", "column")
@@ -13,22 +21,18 @@ var distSVG = d3
 	.attr("width", width)
 	.attr("height", height);
 
-var x = d3.scaleLinear()
-	.range([0, width]),
-
-	y = d3.scaleLinear()
-		.range([0, height]),
-
-	xAxisDist = d3.axisBottom(x)
-		.ticks((width + 2) / (height + 2) * 10)
-		.tickSize(height)
-		.tickPadding(8 - height),
+//configure x,y scales
+var xAxisDist = d3.axisBottom(x)
+	.ticks((width + 2) / (height + 2) * 10)
+	.tickSize(height)
+	.tickPadding(8 - height),
 
 	yAxisDist = d3.axisRight(y)
 		.ticks(10)
 		.tickSize(width)
 		.tickPadding(8 - width);
 
+//append generated x,y scales to main SVG container
 var gX = distSVG.append("g")
 	.attr("class", "axis axis-x-dist")
 	.call(xAxisDist),
@@ -36,16 +40,19 @@ var gX = distSVG.append("g")
 		.attr("class", "axis axis-y-dist")
 		.call(yAxisDist);
 
+//this container holds all the planets
 var planetCont = distSVG
 	.append("g")
 	.attr("transform", "translate(" + 0 + ", " + 0 + ")");
 
+//A color range scale linearly prortional to temperature of planets
 var colorDist = d3
 	.scaleLinear()
 	.domain([0, 400])
 	.interpolate(d3.interpolateHcl)
 	.range([d3.rgb("#007AFF"), d3.rgb("#d80833")]);
 
+//Append Solar system as a reference point
 planetCont
 	.append("circle")
 	.attr("r", 10)
@@ -58,6 +65,7 @@ planetCont
 
 var zoom_handler = d3.zoom().on("zoom", zoom_actions);
 
+//remove and style axis ticks and borders
 function reDraw() {
 	d3.selectAll(".axis-x-dist").selectAll(".tick").select("text").style("fill", "#9b9b9b")
 	d3.selectAll(".axis-x-dist").selectAll(".tick").select("line").style("stroke", "#dddddd")
@@ -71,6 +79,7 @@ function reDraw() {
 
 reDraw()
 
+//this stuff will happend on every tick when zoomed by scoll or double clicked
 function zoom_actions() {
 	planetCont.attr("transform", d3.event.transform);
 	gX.call(xAxisDist.scale(d3.event.transform.rescaleX(x)));
@@ -80,6 +89,7 @@ function zoom_actions() {
 
 zoom_handler(distSVG);
 
+//gets the zoomed in coords with respenct to the object selected. got this from d3 community
 getZoomedCoords = function (W, H, center, w, h, margin) {
 	var k, kh, kw, x, y;
 	kw = (W - margin) / w;
@@ -90,18 +100,32 @@ getZoomedCoords = function (W, H, center, w, h, margin) {
 	return d3.zoomIdentity.translate(x, y).scale(k);
 };
 
-
+//acData holds System names and System data holds System name with properties
+/*
+acData = [sysname1, sysnam2, sysname3, ....]
+SystemData = 
+{
+	sysname1: 
+	[
+	{key-value pairs of planet1 params and its name corresponding to that system},
+	{key-value pairs of planet2 params and its name corresponding to that system}
+	.....
+	]
+}
+*/
 var acData = []; var SystemData = []
+//read generated file by pandas python
 d3.csv("./cartesian-data/cart.csv", function (error, data) {
 	if (error) {
 		console.error(error);
 	}
-
+	//build SystemData JSON
 	groupBy(data, "SystemName", true, function (d) {
 		SystemData = d
 	})
 	//console.log(SystemData)
 
+	//build acData array
 	var data = groupBySpec(data, function (item) {
 		if (!acData.includes(item.SystemName)) {
 			acData.push(item.SystemName);
@@ -109,6 +133,7 @@ d3.csv("./cartesian-data/cart.csv", function (error, data) {
 		return [item.SystemName];
 	});
 
+	//form circles where its radius is proprtional to its z-cordinate in x,y plane with solar system as origin
 	var planets = planetCont
 		.append("g")
 		.attr("class", "planet")
@@ -147,10 +172,13 @@ d3.csv("./cartesian-data/cart.csv", function (error, data) {
 				.call(zoom_handler.transform, transform)
 			var attrName = d3.select(this).attr("name");
 			drawSystem(attrName, SystemData[attrName])
+
 		});
+	//default System
 	drawSystem("TRAPPIST-1", SystemData["TRAPPIST-1"])
 });
 
+//reset zoom function
 function reset() {
 	d3.select("#dist-chart")
 		.select("svg")
@@ -159,6 +187,7 @@ function reset() {
 		.call(zoom_handler.transform, d3.zoomIdentity)
 }
 
+//this function zooms to a particular planet
 function zoomTo(planetName) {
 	findPlanetbyHTML(planetName, distSVG, "circle", "name", function (d) {
 		var transform = getZoomedCoords(
@@ -176,6 +205,7 @@ function zoomTo(planetName) {
 	});
 }
 
+//initialize autocomplente.js using data from acData
 var ac = new autoComplete({
 	selector: 'input[name="search"]',
 	minChars: 1,
@@ -190,6 +220,7 @@ var ac = new autoComplete({
 	offsetTop: 15
 });
 
+//this is just helper function that verifies if planet entered is in acData and prompts user
 function locate() {
 	let pname = document.getElementsByClassName("planet-name-input")[0].value;
 	if (acData.includes(pname)) {
@@ -198,14 +229,18 @@ function locate() {
 	}
 }
 
-////////////////////////////////////
+////////////////////////////////////////
+//This is the System sim SVG container
 
 var simWidth = 350, simHeight = 350;
 
+//handles NaN and returns default value if NaN
 function handleNaN(val, defaultVal) {
 	return isNaN(val) || val == 0 ? defaultVal : val
 }
 
+//orbital velocity of planet calculated using kepler's 3rd law and basic circular motion
+//v = 2*pi*a/T
 function calculateSpeed(period) {
 	if (handleNaN(period, 0) == 0) {
 		return 0.1
@@ -214,6 +249,7 @@ function calculateSpeed(period) {
 	return (2 * Math.PI * a) / period
 }
 
+//A modular d3 functin that takes in planetname and its SystemData and spits out a SVG container with a planet sim
 function drawSystem(planetName, SystemData) {
 
 	d3.select("#planet-sim").select("svg").remove();
@@ -246,12 +282,14 @@ function drawSystem(planetName, SystemData) {
 		.domain([0, 5])
 		.range([0, simWidth / 2]).clamp(true)
 
+	//this is a smallsystem flag, small systems are hard to simulate hance they have seperate scales.
 	var isSmallSystem = false, distSun;
 
 	var distScaleLow = d3.scaleLinear().domain([0, 1]).range([0, simWidth / 2])
 
 	var distScalehz = d3.scaleLinear().domain([0, 30]).range([0, simWidth / 2])
 
+	//read data from main given csv to find more about a planet and to calculate habitable zone
 	d3.csv("./given-data/oec.csv", function (error, data) {
 		if (error) {
 			console.error(error);
@@ -280,7 +318,9 @@ function drawSystem(planetName, SystemData) {
 			})
 		});
 
+		//only calculaye hz if data is given else don't since its expensive
 		if (!+starTemp == 0 && !+starRad == 0) {
+			//calculate habitable zome (heavy computation ahead)
 			hzCalculated = true;
 			luminosity = ((starRad / 695700) ** 2) * ((starTemp / 5778) ** 4)
 			absLuminosity = Math.log10(luminosity) * -2.5
@@ -300,10 +340,12 @@ function drawSystem(planetName, SystemData) {
 
 		}
 
+		//append host star to sim
 		sim.append("circle").attr("r", handleNaN(radScale(starRad / 696000), 15)).attr("cx", simWidth / 2)
 			.attr("cy", simHeight / 2).attr("class", "host")
 			.attr("style", "fill: #FFA500; filter:url(#sun-grad)")
 
+		//consider all edge cases like S,P binary and orphan stars, since their models tend to be mostly inaccurate
 		switch (type) {
 			case 1: var t = sim.append("text").attr("x", 100).attr("y", 100).attr("opacity", 0.5)
 				t.append("tspan").attr("x", 70).attr("dy", "1.2em").text("This is a P-Type Binary System.")
@@ -327,6 +369,7 @@ function drawSystem(planetName, SystemData) {
 		var simCont = sim.append("g")
 			.attr("transform", "translate(" + simWidth / 2 + "," + simHeight / 2 + ")")
 
+		//append planets to sim and do some easing on the orbits
 		simCont.selectAll("g.planet").data(planets).enter().append("g")
 			.attr("class", "planet").each(function (d, i) {
 				d3.select(this).append("circle").attr("class", "planet-sim-orbit").attr("fill", "none").attr("stroke", "#dddddd").transition().duration(600).ease(d3.easeCubicInOut)
@@ -335,12 +378,14 @@ function drawSystem(planetName, SystemData) {
 					.attr("cy", 0).attr("class", "planet-sim-planet").attr("style", "fill: " + d.color + "; filter:url(#glow)")
 			});
 
+		//revolve around host star
 		d3.timer(function () {
 			var delta = (Date.now() - timeInit);
 			sim.selectAll(".planet").attr("transform", function (d) {
 				return "rotate(" + (d.angle + delta * (d.speed / 100)) % 360 + ")";
 			});
 		});
+
 		//////////////////////update planet-info
 		let planetInfo = d3.select(".info-list"),
 			planetArr = [];
